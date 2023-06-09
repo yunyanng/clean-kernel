bdc10effa50841cc98afcf74a6cca67a31414a41
YunYan Ng <35233577+yunyanng@users.noreply.github.com>
2023-06-09 10:02:55 +0800

    feat: replace fluentd with fluent bit

--- dotnet/src/API/CleanKernel.API/Extensions/SerilogExtensions.cs
+++ dotnet/src/API/CleanKernel.API/Extensions/SerilogExtensions.cs
@@ -6,14 +6,14 @@ public static class SerilogExtensions
 {
     public static void ConfigureSerilog([NotNull] this WebApplicationBuilder builder, string appName)
     {
-        var fluentdUrl = builder.Configuration["Serilog:FluentdUrl"];
+        var fluentBitUrl = builder.Configuration["Serilog:FluentBitUrl"];
 
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Verbose()
             .Enrich.WithProperty("ApplicationName", appName)
             .Enrich.FromLogContext()
             .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture)
-            .WriteTo.Http(string.IsNullOrWhiteSpace(fluentdUrl) ? $"http://fluentd:9880/{WebUtility.UrlEncode(appName)}" : fluentdUrl, null)
+            .WriteTo.Http(string.IsNullOrWhiteSpace(fluentBitUrl) ? $"http://fluent-bit:9880/{WebUtility.UrlEncode(appName)}" : fluentBitUrl, null)
             .ReadFrom.Configuration(builder.Configuration)
             .CreateLogger();
 
